503ef1ee59539d5d066367f65adece5bac30d14a
suryaprakash-s-sys <suryaprakash.252004@gmail.com>
2026-06-19 17:43:11 +0530

    Signals and Directives addon

---
+++ Workspace/myApp/src/app/01-signals/counter/counter.ts
@@ -0,0 +1,14 @@
+import { CommonModule } from '@angular/common';
+import { Component,} from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-counter',
+  imports: [CommonModule,FormsModule],
+  templateUrl: './counter.html',
+  styleUrl: './counter.css',
+})
+export class Counter 
+{
+    showme=true;
+}
--- Workspace/myApp/src/app/app.ts
+++ Workspace/myApp/src/app/app.ts
@@ -1,21 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { Counter } from './01-signals/counter/counter';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FormsModule],
+  imports: [RouterOutlet, FormsModule, Counter],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 
 export class App 
  {
-    clickcount=0;
-
-    click()
-    {
-      this.clickcount++;
-    }
+  
 }
